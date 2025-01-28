0c28e5f2eca067eec13a8ad1bdb13eb004cddc1f
LaulinVasquez <427339400@byui.edu>
2025-01-28 13:41:46 -0700

    moving everything out of disk

--- src/components/Title/Title.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Title.module.css";
-import Typography from "../Typography/Typography";
-function Title() {
-  return (
-    <div className={styles.titleContainer}> <Typography fontSize={30} bold>To do List</Typography> </div>
-  )
-}
-
-export default Title
